ed39665a0b7bebcced97121ac0cfd80efc6d8685
kacluc <149156481+kacluc@users.noreply.github.com>
2026-03-09 14:24:20 +0100

    [Lab3.2] done

--- vod-app/src/main/java/vod/web/rest/RestaurantRest.java
+++ vod-app/src/main/java/vod/web/rest/RestaurantRest.java
@@ -1,16 +1,22 @@
 package vod.web.rest;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.LocaleResolver;
 import vod.model.Dish;
 import vod.model.Restaurant;
 import vod.service.DishService;
 import vod.service.RestaurantService;
 
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,12 +25,18 @@ import java.util.List;
 public class RestaurantRest {
     private final RestaurantService restaurantService;
     private final DishService dishService;
+    private final MessageSource messageSource;
+    private final LocaleResolver localeResolver;
 
     @GetMapping("/restaurants")
     List<Restaurant> getRestaurants(@RequestParam(value = "phrase", required = false) String phrase,
-                                    @RequestHeader(value = "custom-header", required = false) String customHeader
-                                    ) {
+                                    @RequestHeader(value = "custom-header", required = false) String customHeader,
+                                    @CookieValue(value = "some-cookie", required = false) String someCookie
+    ) {
         log.info("about to retrieve restaurants list");
+        log.info("phrase: {}", phrase);
+        log.info("custom-header: {}", customHeader);
+        log.info("some-cookie: {}", someCookie);
         List<Restaurant> restaurants = restaurantService.getAllRestaurants();
         log.info("{} restaurants found: ", restaurants.size());
         return restaurants;
@@ -56,9 +68,17 @@ public class RestaurantRest {
     }
 
     @PostMapping("/restaurants")
-    ResponseEntity<Restaurant> addRestaurant(@RequestBody Restaurant restaurant) {
+    ResponseEntity<?> addRestaurant(@Validated @RequestBody Restaurant restaurant, Errors errors, HttpServletRequest request) {
         log.info("adding restaurant " + restaurant);
-        //TODO validation
+
+        if(errors.hasErrors()) {
+            Locale locale = localeResolver.resolveLocale(request);
+            String errorMessage = errors.getAllErrors().stream()
+                    .map(oe -> messageSource.getMessage(oe.getCode(), new Object[0], locale))
+                    .reduce("error:\n", (accu,oe)  -> accu + oe + "\n");
+            return ResponseEntity.badRequest().body(restaurant);
+        }
+
         restaurant = restaurantService.addRestaurant(restaurant);
         log.info("new restaruant added: ", restaurant);
         return ResponseEntity.status(HttpStatus.CREATED).body(restaurant);
